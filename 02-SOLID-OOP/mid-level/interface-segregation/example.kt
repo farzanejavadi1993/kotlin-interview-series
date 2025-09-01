99e6db9e891152bb832361692fb14a69fd973d00
Farzane Javadi <111173799+farzanejavadi1993@users.noreply.github.com>
2025-09-01 21:25:51 +0200

    Update example.kt

--- 02-SOLID-OOP/mid-level/interface-segregation/example.kt
+++ 02-SOLID-OOP/mid-level/interface-segregation/example.kt
@@ -1,13 +1,31 @@
-interface OnClickListener {
+```kotlin
+// Bad example: one huge interface
+interface UserActions {
+    fun onClick()
+    fun onSwipe()
+    fun onZoom()
+}
+
+class PhotoViewer : UserActions {
+    override fun onClick() = println("Clicked")
+    override fun onSwipe() = println("Swiped")
+    override fun onZoom() = println("Zoomed")
+}
+
+// Better: small, focused interfaces
+interface Clickable {
     fun onClick()
 }
 
-interface OnSwipeListener {
+interface Swipable {
     fun onSwipe()
 }
 
-class SwipeGestureDetector : OnSwipeListener {
-    override fun onSwipe() {
-        // Do swipe logic
-    }
+interface Zoomable {
+    fun onZoom()
+}
+
+class ImageViewer : Swipable, Zoomable {
+    override fun onSwipe() = println("Swiped image")
+    override fun onZoom()  = println("Zoomed image")
 }
